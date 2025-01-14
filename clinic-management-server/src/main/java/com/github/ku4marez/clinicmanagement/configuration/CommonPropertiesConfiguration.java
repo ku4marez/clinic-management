98d53a32f5b9abce4b572b8bd862819b0d84002f
Anton Kucha <anton.kucha@aionys.com>
2025-01-14 23:22:20 +0100

    Extract common properties + docker compose

---
+++ clinic-management-server/src/main/java/com/github/ku4marez/clinicmanagement/configuration/CommonPropertiesConfiguration.java
@@ -0,0 +1,9 @@
+package com.github.ku4marez.clinicmanagement.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application-common.properties")
+public class CommonPropertiesConfiguration {
+}
