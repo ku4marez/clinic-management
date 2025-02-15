f12c13eb91e90d77b033dbca21b558c0b527777d
Anton Kucha <anton.kucha@aionys.com>
2025-02-15 21:22:13 +0100

    Fix server startup errors

--- clinic-management-server/src/main/java/com/github/ku4marez/clinicmanagement/configuration/AppConfiguration.java
+++ clinic-management-server/src/main/java/com/github/ku4marez/clinicmanagement/configuration/AppConfiguration.java
@@ -3,6 +3,7 @@ package com.github.ku4marez.clinicmanagement.configuration;
 import com.github.ku4marez.commonlibraries.util.AuditorAwareUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.security.core.Authentication;
@@ -11,14 +12,16 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@EnableMongoAuditing
+@EnableMongoAuditing(auditorAwareRef = "auditorProvider")
 public class AppConfiguration {
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
+    @Primary
     public AuditorAware<String> auditorProvider() {
         return new AuditorAwareUtil(this::getCurrentUser);
     }
